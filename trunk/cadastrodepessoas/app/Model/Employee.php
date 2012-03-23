fe7d7fd3a04b72d46ce6325428936c936e6322f9
thisilva@gmail.com <thisilva@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-03-23 17:38:42 +0000

    relacionamentos

--- trunk/cadastrodepessoas/app/Model/Employee.php
+++ trunk/cadastrodepessoas/app/Model/Employee.php
@@ -1,4 +1,6 @@
 <?php
 class Employee extends AppModel {
 	public $name = 'Employee';
+	
+	public $belongsTo = array('User' => array('className' => 'User', 'foreignKey' => 'user_id'));
 }
--- trunk/cadastrodepessoas/app/Model/Student.php
+++ trunk/cadastrodepessoas/app/Model/Student.php
@@ -1,4 +1,6 @@
 <?php
 class Student extends AppModel {
 	public $name = 'Student';
+	
+	public $belongsTo = array('User' => array('className' => 'User', 'foreignKey' => 'user_id'));
 }
