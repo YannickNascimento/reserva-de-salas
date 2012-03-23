640329118ab5737545699afe11fc895e8c63ca47
thisilva@gmail.com <thisilva@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-03-23 17:38:42 +0000

    relacionamentos

--- cadastrodepessoas/app/Model/Professor.php
+++ cadastrodepessoas/app/Model/Professor.php
@@ -1,4 +1,6 @@
 <?php
 class Professor extends AppModel {
 	public $name = 'Professor';
+	
+	public $belongsTo = array('User' => array('className' => 'User', 'foreignKey' => 'user_id'));
 }
--- cadastrodepessoas/app/Model/User.php
+++ cadastrodepessoas/app/Model/User.php
@@ -1,4 +1,8 @@
 <?php
 class User extends AppModel {
 	public $name = 'User';
+
+	public $hasOne = array('Student' => array('className' => 'Student', 'dependent' => 'true'),
+					'Professor' => array('className' => 'Professor', 'dependent' => 'true'),
+					'Employee' => array('className' => 'Employee', 'dependent' => 'true'));
 }
