d8c910d864471adc44ddd1cb2fa159b15359fc49
claudivan.rbr@gmail.com <claudivan.rbr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-03-28 17:43:45 +0000

    Salvando perfil dos usuarios

--- cadastrodepessoas/app/Controller/UsersController.php
+++ cadastrodepessoas/app/Controller/UsersController.php
@@ -1,4 +1,8 @@
 <?php
+App::uses('Student', 'Model');
+App::uses('Professor', 'Model');
+App::uses('Employee', 'Model');
+
 class UsersController extends AppController {
 	public $name = 'Users';
 
@@ -6,12 +10,34 @@ class UsersController extends AppController {
 
 	public function beforeFilter() {
 		$this->Auth->allow(array('createAccount'));
+
+		$this->Student = ClassRegistry::init('Student');
+		$this->Professor = ClassRegistry::init('Professor');
+		$this->Employee = ClassRegistry::init('Employee');
 	}
 
 	public function createAccount() {
 		if ($this->request->is('post')) {
 			if ($this->User->save($this->request->data)) {
 				$this->Session->setFlash(__('Email enviado para validação'));
+
+				switch ($this->request->data['User']['userType']) {
+				case 'professor':
+					$this->Professor
+							->saveProfile($this->User->id, $this->request->data);
+					break;
+				case 'student':
+					$this->Student
+							->saveProfile($this->User->id, $this->request->data);
+					break;
+				case 'employee':
+					$this->Professor
+							->saveProfile($this->User->id, $this->request->data);
+					break;
+				default:
+					$this->Session
+							->setFlash(__('E#2: Erro ao cadastrar perfil'));
+				}
 
 				$user = $this->User->findById($this->User->id);
 
@@ -22,7 +48,7 @@ class UsersController extends AppController {
 								array('controller' => 'Users',
 										'action' => 'login'));
 			} else {
-				$this->Session->setFlash(__('Erro ao cadastrar conta'));
+				$this->Session->setFlash(__('E#1: Erro ao cadastrar conta'));
 			}
 		}
 	}
--- cadastrodepessoas/app/Model/Student.php
+++ cadastrodepessoas/app/Model/Student.php
@@ -4,4 +4,10 @@ class Student extends AppModel {
 
 	public $belongsTo = array(
 			'User' => array('className' => 'User', 'foreignKey' => 'user_id'));
+
+	public function saveProfile($id, $data) {
+		$data['Student']['user_id'] = $id;
+
+		$this->save($data);
+	}
 }
