56b0f15adae63530ebb581be01f0cb9728b6f174
alberto.bueno.jr@gmail.com <alberto.bueno.jr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-05-09 19:07:33 +0000

    Finalizado cadastro de Salas e Recursos.

--- trunk/reservadesalas/app/Model/Resource.php
+++ trunk/reservadesalas/app/Model/Resource.php
@@ -4,4 +4,14 @@ class Resource extends AppModel {
 
 	public $belongsTo = array(
 			'Room' => array('className' => 'Room', 'foreignKey' => 'room_id'));
+	
+	public $validate = array(
+			'name' => array(
+					'not empty' => array('required' => true,
+							'rule' => 'notEmpty',
+							'message' => 'Não deve ser vazio.')),
+			'serial_number' => array(
+					'not empty' => array('required' => true,
+							'rule' => 'notEmpty',
+							'message' => 'Não deve ser vazio.')));
 }
---
+++ trunk/reservadesalas/app/Scripts/createRooms.sql
@@ -0,0 +1,9 @@
+USE reservadesalas
+
+INSERT INTO rooms(building_id, name, number, floor, room_type, description, capacity) VALUES
+(1, 'Antonio Gilioli', 120, 1, 'auditorium', 'Sala bonita', 100),
+(1, 'Corredor', 125, 1, 'normal', 'Sala da Rede Linux', 30),
+(1, 'Herois', 126, 1, 'normal', 'Sala da Rede Linux', 30),
+(1, 'Aquario', 122, 1, 'normal', 'Sala da Rede Linux', 30),
+(1, 'BCC', 258, 1, 'normal', 'Sala da Rede Linux', 30),
+(2, 'CEC Principal', 10, 0, 'normal', 'Laboratório de XP', 30);
