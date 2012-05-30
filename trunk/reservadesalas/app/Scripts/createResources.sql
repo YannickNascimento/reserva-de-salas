187b18c45da917ddee07b519a0678171fa62b459
alberto.bueno.jr@gmail.com <alberto.bueno.jr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-05-30 17:32:32 +0000

    SQL Create Resources

---
+++ trunk/reservadesalas/app/Scripts/createResources.sql
@@ -0,0 +1,11 @@
+USE reservadesalas
+
+INSERT INTO resources(room_id, serial_number, name, description) VALUES
+(NULL, '111111', 'Projetor HP', 'Projetor 1'),
+(1, '222222', 'Projetor HP', 'Projetor 2'),
+(2, '333333', 'Projetor HP', 'Projetor 3'),
+(NULL, '444444', 'Projetor HP', 'Projetor 4'),
+(NULL, '555555', 'Projetor HP', 'Projetor 5'),
+(1, '6666666', 'Ar Condicionado', 'Super gelado'),
+(2, '7777777', 'Ar Condicionado', 'Super quente'),
+(NULL, '88888', 'Ar Condicionado', 'Super Quebrado');
--- trunk/reservadesalas/app/Scripts/createRooms.sql
+++ trunk/reservadesalas/app/Scripts/createRooms.sql
@@ -1,9 +1,9 @@
 USE reservadesalas
 
-INSERT INTO rooms(building_id, name, number, floor, room_type, description, capacity) VALUES
-(1, 'Antonio Gilioli', 120, 1, 'auditorium', 'Sala bonita', 100),
-(1, 'Corredor', 125, 1, 'normal', 'Sala da Rede Linux', 30),
-(1, 'Herois', 126, 1, 'normal', 'Sala da Rede Linux', 30),
-(1, 'Aquario', 122, 1, 'normal', 'Sala da Rede Linux', 30),
-(1, 'BCC', 258, 1, 'normal', 'Sala da Rede Linux', 30),
-(2, 'CEC Principal', 10, 0, 'normal', 'Laboratório de XP', 30);
+INSERT INTO rooms(id, building_id, name, number, floor, room_type, description, capacity) VALUES
+(1, 1, 'Antonio Gilioli', 120, 1, 'auditorium', 'Sala bonita', 100),
+(2, 1, 'Corredor', 125, 1, 'normal', 'Sala da Rede Linux', 30),
+(3, 1, 'Herois', 126, 1, 'normal', 'Sala da Rede Linux', 30),
+(4, 1, 'Aquario', 122, 1, 'normal', 'Sala da Rede Linux', 30),
+(5, 1, 'BCC', 258, 1, 'normal', 'Sala da Rede Linux', 30),
+(6, 2, 'CEC Principal', 10, 0, 'normal', 'Laboratório de XP', 30);
