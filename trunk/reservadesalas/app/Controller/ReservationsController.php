17b8231a22120605b2efa84d440e213178699969
alberto.bueno.jr@gmail.com <alberto.bueno.jr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-05-30 19:04:34 +0000

    Pagina de criacao de reservas feita

--- trunk/reservadesalas/app/Controller/ReservationsController.php
+++ trunk/reservadesalas/app/Controller/ReservationsController.php
@@ -18,6 +18,12 @@ class ReservationsController extends AppController {
 
 	public function chooseDate() {
 	}
+	
+	public function createReservation($roomId, $startDatetime, $endDatetime) {
+		// DATETIME FORMAT: yyyy-mm-dd hh:mm:ss
+		//$t1 = "2012-06-30 12:00:00";
+		//$t2 = "2012-06-30 17:00:00";
+	}
 
 	public function loadAvailableRooms() {
 		$this->RequestHandler->respondAs('json');
---
+++ trunk/reservadesalas/app/webroot/js/create_reservation.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	$("#addResource").click(function() {
+		var addResourceButton = $(this);
+		$.ajax({
+           type: 'POST',
+           dataType: 'json',
+           url: '/reservadesalas/Resources/getAvailableResources',
+           data: {startDatetime: "lala", endDatetime: "lala"},
+           success: function(data) {
+               $('#availableResources').html("");
+               var resources = $("#resources").find('table');
+               var table = $("<table id='roomsTable'></table>");
+               var header = $("<tr><td class='header'>Nome</td><td class='header'>Número de Série</td><td class='header'></td></tr>");
+               table.append(header);
+               
+               $.each(data, function(i, value) {
+            	   var name = value['Resource']['name'];
+            	   var serial_number = value['Resource']['serial_number'];
+            	   var resourceId = value['Resource']['id'];
+            	   var row = $("<tr><td><input type='hidden' value='" + value['Resource']['id'] + "' />" + name + "</td><td>" + serial_number + "</td><td class='center'><button type='button' class='select'>Adicionar</button></td></tr>");
+            	   row.find(".select").click(function() {
+            		   var addButton = $(this);
+            		   var line = $("<tr></tr>");
+            		   var input = $("<input type='hidden' name='data[Reservation][resources][]' value='" + resourceId + "' />");
+            		   line.append(input);
+            		   var closeButton = $("<button>Remover</button>");
+            		   closeButton.click(function() {
+            			  line.remove();
+            			  addButton.show();
+            		   });
+            		   line.append("<td>" + name + "</td><td>" + serial_number + "</td>");
+            		   var cell = $("<td class='center'></td>");
+            		   cell.append(closeButton);
+            		   line.append(cell);
+            		   resources.append(line);
+            		   addButton.hide();
+            	   });
+            	   table.append(row);
+               });
+               addResourceButton.remove();
+               $("#availableResources").append("<h2>Recursos disponíveis</h2>")
+               $('#availableResources').append(table);
+           }
+        });
+		return false;
+	})
+});
