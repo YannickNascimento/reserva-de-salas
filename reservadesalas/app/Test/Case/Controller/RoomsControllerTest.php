b11017fbb8144e46fa8e95f832ec67a4ddb7b6c8
alberto.bueno.jr@gmail.com <alberto.bueno.jr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-05-30 17:13:50 +0000

    Testes 100porcento!

--- reservadesalas/app/Test/Case/Controller/RoomsControllerTest.php
+++ reservadesalas/app/Test/Case/Controller/RoomsControllerTest.php
@@ -1,12 +1,19 @@
 <?php
 App::uses('User', 'Model');
+App::uses('Student', 'Model');
+App::uses('Professor', 'Model');
+App::uses('Employee', 'Model');
 App::uses('Building', 'Model');
 App::uses('Room', 'Model');
 App::uses('Resource', 'Model');
 App::uses('RoomsController', 'Controller');
+App::uses('UsersControllerTest', 'Test/Case/Controller');
 
 class RoomsControllerTest extends ControllerTestCase {
-	public $fixtures = array('app.user', 'app.building', 'app.room', 'app.resource');
+	public $fixtures = array('app.user', 'app.student', 'app.professor',
+			'app.employee', 'app.course', 'app.department',
+			'app.professorCategory', 'app.building', 'app.room',
+			'app.resource');
 
 	public function setUp() {
 		parent::setUp();
@@ -19,111 +26,168 @@ class RoomsControllerTest extends ControllerTestCase {
 		$this->RoomsController = new RoomsController();
 		$this->RoomsController->constructClasses();
 	}
-	
+
 	public function testGetCreateRoom() {
 		$this->testAction('/Rooms/createRoom/' . 1, array('method' => 'get'));
 	}
-	
+
 	public function testCreateRoom() {
 		$data = array(
-				'Room' => array('building_id' => 3,
-					'name' => 'Jacy Monteiro',
-					'number' => 100,
-					'floor' => 0,
-					'room_type' => 'auditorium',
-					'description' => 'Auditorio legal',
-					'capacity' => 100
-					));
+				'Room' => array('building_id' => 3, 'name' => 'Jacy Monteiro',
+						'number' => 100, 'floor' => 0,
+						'room_type' => 'auditorium',
+						'description' => 'Auditorio legal', 'capacity' => 100));
 
-				
 		$this
 				->testAction('Rooms/createRoom',
 						array('method' => 'post', 'data' => $data));
-		
+
 		$this->Room->order = 'Room.id DESC';
 		$room = $this->Room->find('first');
 
-		$this->assertEqual($room['Room']['building_id'], $data['Room']['building_id']);
+		$this
+				->assertEqual($room['Room']['building_id'],
+						$data['Room']['building_id']);
 		$this->assertEqual($room['Room']['name'], $data['Room']['name']);
 		$this->assertEqual($room['Room']['number'], $data['Room']['number']);
 		$this->assertEqual($room['Room']['floor'], $data['Room']['floor']);
-		$this->assertEqual($room['Room']['room_type'], $data['Room']['room_type']);
-		$this->assertEqual($room['Room']['description'], $data['Room']['description']);
-		$this->assertEqual($room['Room']['capacity'], $data['Room']['capacity']);
+		$this
+				->assertEqual($room['Room']['room_type'],
+						$data['Room']['room_type']);
+		$this
+				->assertEqual($room['Room']['description'],
+						$data['Room']['description']);
+		$this
+				->assertEqual($room['Room']['capacity'],
+						$data['Room']['capacity']);
 	}
-	
+
 	public function testCreateInvalidRoom() {
-			$this->Room->order = 'Room.id DESC';
-			$last_room = $this->Room->find('first');
-			$data = array(
-					'Room' => array('building_id' => NULL,
-						'name' => 'Jacy Monteiro',
-						'number' => 100,
-						'floor' => NULL,
-						'room_type' => NULL,
-						'description' => 'Auditorio legal',
-						'capacity' => NULL
-						));
-	
-			$this
-					->testAction('Rooms/createRoom',
-							array('method' => 'post', 'data' => $data));
-			$this->Room->order = 'Room.id DESC';
-			$room = $this->Room->find('first');
-			
-	
-			$this->assertEqual($room['Room']['building_id'], $last_room['Room']['building_id']);
-			$this->assertEqual($room['Room']['name'], $last_room['Room']['name']);
-			$this->assertEqual($room['Room']['number'], $last_room['Room']['number']);
-			$this->assertEqual($room['Room']['floor'], $last_room['Room']['floor']);
-			$this->assertEqual($room['Room']['room_type'], $last_room['Room']['room_type']);
-			$this->assertEqual($room['Room']['description'], $last_room['Room']['description']);
-			$this->assertEqual($room['Room']['capacity'], $last_room['Room']['capacity']);
+		$this->Room->order = 'Room.id DESC';
+		$last_room = $this->Room->find('first');
+		$data = array(
+				'Room' => array('building_id' => NULL,
+						'name' => 'Jacy Monteiro', 'number' => 100,
+						'floor' => NULL, 'room_type' => NULL,
+						'description' => 'Auditorio legal', 'capacity' => NULL));
+
+		$this
+				->testAction('Rooms/createRoom',
+						array('method' => 'post', 'data' => $data));
+		$this->Room->order = 'Room.id DESC';
+		$room = $this->Room->find('first');
+
+		$this
+				->assertEqual($room['Room']['building_id'],
+						$last_room['Room']['building_id']);
+		$this->assertEqual($room['Room']['name'], $last_room['Room']['name']);
+		$this
+				->assertEqual($room['Room']['number'],
+						$last_room['Room']['number']);
+		$this->assertEqual($room['Room']['floor'], $last_room['Room']['floor']);
+		$this
+				->assertEqual($room['Room']['room_type'],
+						$last_room['Room']['room_type']);
+		$this
+				->assertEqual($room['Room']['description'],
+						$last_room['Room']['description']);
+		$this
+				->assertEqual($room['Room']['capacity'],
+						$last_room['Room']['capacity']);
 	}
 
 	public function testGetViewRoom() {
 		$this->testAction('/Rooms/viewRoom/' . 1, array('method' => 'get'));
 	}
-	
+
 	public function testGetViewInvalidRoom() {
-		$this->assertEqual($this->testAction('/Rooms/viewRoom/' . null, array('method' => 'get')), $this->redirect(
-				array('controller' => 'Rooms',
-						'action' => 'listRoom')));
-	}
-	
-	public function testFilterRoomsByName() {
-		$data = array('Room' => array('name' => 'CE'), 'Building'=>array('id'=>''));
-	
 		$this
-		->testAction('Rooms/listRooms',
-				array('method' => 'post', 'data' => $data));
-	
-		$this->assertInternalType('array', $this->vars['rooms']);
-		$this->assertNotEqual($this->vars['rooms'], null);
-	
-		$this->assertEqual(count($this->vars['rooms']), 1);
+				->assertEqual(
+						$this
+								->testAction('/Rooms/viewRoom/' . null,
+										array('method' => 'get')),
+						$this
+								->redirect(
+										array('controller' => 'Rooms',
+												'action' => 'listRoom')));
 	}
-	
+
 	public function testListRooms() {
 		$this->testAction('Rooms/listRooms', array('method' => 'get'));
-	
+
 		$expected_rooms = $this->Room->find('all');
 		$listed_rooms = $this->vars['rooms'];
-		
+
 		$this->assertEqual(count($listed_rooms), count($expected_rooms));
-		
+
 		foreach ($expected_rooms as $room) {
 			$this->assertEqual(in_array($room, $listed_rooms), true);
 		}
-		
-		
+
 		$expected_buildings = $this->Building->find('all');
 		$listed_buildings = $this->vars['buildings'];
-		
-		$this->assertEqual(count($listed_buildings), count($expected_buildings));
-		
+
+		$this
+				->assertEqual(count($listed_buildings),
+						count($expected_buildings));
+
 		foreach ($expected_buildings as $building) {
 			$this->assertEqual(in_array($building, $listed_buildings), true);
 		}
+	}
+
+	public function testGetListRoomsLogged() {
+		UsersControllerTest::loginAsActiveUser();
+		$result = $this
+				->testAction('/Rooms/listRooms',
+						array('method' => 'get', 'return' => 'vars'));
+		$this->assertFalse(isset($this->headers['Location']));
+	}
+
+	public function testGetListRoomsNotLogged() {
+		$result = $this
+				->testAction('/Rooms/listRooms',
+						array('method' => 'get', 'return' => 'vars'));
+		$this->assertContains("/Users/login", $this->headers['Location']);
+	}
+	
+	public function testFilterRoomsByName() {
+		$data = array('Room' => array('name' => 'CE'),
+				'Building' => array('id' => ''));
+
+		$this
+				->testAction('Rooms/listRooms',
+						array('method' => 'post', 'data' => $data));
+
+		$this->assertInternalType('array', $this->vars['rooms']);
+		$this->assertNotEqual($this->vars['rooms'], null);
+
+		$this->assertEqual(count($this->vars['rooms']), 1);
+	}
+	public function testFilterRoomsByCapacityAndBuilding() {
+		$data = array('Room' => array('capacity' => 100, 'floor' => 1),
+				'Building' => array('id' => 1));
+
+		$this
+				->testAction('Rooms/listRooms',
+						array('method' => 'post', 'data' => $data));
+
+		$this->assertInternalType('array', $this->vars['rooms']);
+		$this->assertNotEqual($this->vars['rooms'], null);
+
+		$this->assertEqual(count($this->vars['rooms']), 1);
+	}
+	public function testFilterRoomsByInvalidFloor() {
+		$data = array('Room' => array('floor' => 300),
+				'Building' => array('id' => ''));
+
+		$this
+				->testAction('Rooms/listRooms',
+						array('method' => 'post', 'data' => $data));
+
+		$this->assertInternalType('array', $this->vars['rooms']);
+		$this->assertNotEqual($this->vars['rooms'], null);
+
+		$this->assertEqual(count($this->vars['rooms']), 0);
 	}
 }
