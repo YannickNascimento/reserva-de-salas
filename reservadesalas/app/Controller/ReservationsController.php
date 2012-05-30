671261101a25014b01eb4a76f5427b1089ce8722
claudivan.rbr@gmail.com <claudivan.rbr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-05-30 17:58:33 +0000

    Ajax funcionando.

--- reservadesalas/app/Controller/ReservationsController.php
+++ reservadesalas/app/Controller/ReservationsController.php
@@ -1,16 +1,32 @@
 <?php
+App::uses('Room', 'Model');
+
 class ReservationsController extends AppController {
 	public $name = 'Reservations';
 
+	public $components = array('RequestHandler');
+
 	public function beforeFilter() {
 		parent::beforeFilter();
+
+		$this->Room = ClassRegistry::init('Room');
 	}
 
 	public function isAuthorized($user) {
 		return parent::isAuthorized($user);
 	}
-	
+
 	public function chooseDate() {
+	}
+
+	public function loadAvailableRooms() {
+		$this->RequestHandler->respondAs('json');
+		$this->autoRender = false;
+
+		$rooms = $this->Room->find('all');
+
+		echo json_encode($rooms);
+		exit();
 	}
 }
 ?>
--- reservadesalas/app/webroot/js/choose_date.js
+++ reservadesalas/app/webroot/js/choose_date.js
@@ -5,23 +5,36 @@ $(document).ready(function() {
 	
 	$("#DateEndTime").timepicker({});
 	
+	
 	$("#loadAvailableRooms").click(function(){
 		/* TODO: make controller */
 		var date = $("#DateDate").val();
 		var begin_time = $("#DateBeginTime").val();
 		var end_time = $("#DateEndTime").val();
+		
+		if (date == '') {
+			$('#DateDate').parent().addClass('error');
+			$('#DateDate').parent().append('<div class=\'error-message\'>Não deve ser vazio.</div>');
+			return false;
+		}
+		
 		$.ajax({
            type: 'POST',
            dataType: 'json',
-           url: '/Reservations/loadAvailableRooms',
-           data: {date: date, begin_time: begin_time, end_time: end_time},
+           url: '/reservadesalas/Reservations/loadAvailableRooms',
+           data: {},
            success: function(data) {
                $('#availableRooms').html('');
-               options = '';
+               options = '<br />\
+            	   			<table id=\'roomsTable\'>\
+            	   		      <tr><td class=\'header\'>Nome</td><td class=\'header\'>Número</td><td class=\'header\'>Bloco</td></tr>';
                for (i in data) {
-                   room = data[i];
-                   options += '<p>'+ room +'</p>';
+            	   name = (data[i]['Room']['name'] == null) ? '': data[i]['Room']['name'];
+                   number = data[i]['Room']['number'];
+                   building = data[i]['Building']['name'];
+                   options += '<tr><td>'+ name + '</td><td>' + number + '</td><td>' + building + '</td><tr>';
                }
+               options += '</table>';
                $('#availableRooms').html(options);
            }
         });
