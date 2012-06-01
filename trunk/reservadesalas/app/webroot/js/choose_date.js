e3af194712edde6f8e8e5bf8604868ec1b652cfb
claudivan.rbr@gmail.com <claudivan.rbr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-06-01 16:53:48 +0000

    Tela de listagem de salas pra reserva quase funcionando.

--- trunk/reservadesalas/app/webroot/js/choose_date.js
+++ trunk/reservadesalas/app/webroot/js/choose_date.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
 	
 	$("#DateEndTime").timepicker({});
 	
+	function validateInputItem(inputID, value) {
+		$(inputID).parent().removeClass('error');
+		$(inputID).next('.error-message').remove();
+		
+		if (value == '') {
+			$(inputID).parent().addClass('error');
+			$(inputID).parent().append('<div class=\'error-message\'>Não deve ser vazio.</div>');
+			return false;
+		}
+		
+		return true;
+	}
 	
 	$("#loadAvailableRooms").click(function(){
 		/* TODO: make controller */
@@ -12,18 +24,19 @@ $(document).ready(function() {
 		var begin_time = $("#DateBeginTime").val();
 		var end_time = $("#DateEndTime").val();
 		
-		if (date == '') {
-			$('#DateDate').parent().addClass('error');
-			$('#DateDate').parent().append('<div class=\'error-message\'>Não deve ser vazio.</div>');
+		if (validateInputItem('#DateDate', date) == false |
+			validateInputItem('#DateBeginTime', begin_time) == false |
+			validateInputItem('#DateEndTime', end_time) == false)
 			return false;
-		}
 		
+		var json = $.toJSON({'date': date, 'begin_time': begin_time, 'end_time': end_time});
 		$.ajax({
            type: 'POST',
            dataType: 'json',
            url: '/reservadesalas/Reservations/loadAvailableRooms',
-           data: {},
+           data: {data: json},
            success: function(data) {
+        	   alert(data);
                $('#availableRooms').html('');
                options = '<br />\
             	   			<table id=\'roomsTable\'>\
@@ -36,6 +49,9 @@ $(document).ready(function() {
                }
                options += '</table>';
                $('#availableRooms').html(options);
+           },
+           error: function(jqXHR, textStatus, errorThrown){
+        	   alert ('Something very bad went wrong: ' + textStatus);
            }
         });
 		return false;
