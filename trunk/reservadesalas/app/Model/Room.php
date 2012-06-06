1910bb4561d72af337fd4455a1f0021cb3ebe267
alberto.bueno.jr@gmail.com <alberto.bueno.jr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-06-06 17:58:03 +0000

    Validação da reserva

--- trunk/reservadesalas/app/Model/Room.php
+++ trunk/reservadesalas/app/Model/Room.php
@@ -1,11 +1,13 @@
 <?php
+App::uses('Reservation', 'Model');
+
 class Room extends AppModel {
 	public $name = 'Room';
 
 	public $belongsTo = array(
 			'Building' => array('className' => 'Building',
 					'foreignKey' => 'building_id'));
-	
+
 	public $validate = array(
 			'floor' => array(
 					'not empty' => array('required' => true,
@@ -18,10 +20,25 @@ class Room extends AppModel {
 			'capacity' => array(
 					'not empty' => array('required' => true,
 							'rule' => 'notEmpty',
-							'message' => 'Não deve ser vazio.'))
-					
-			);
+							'message' => 'Não deve ser vazio.')));
 
 	public $hasMany = array(
 			'Resources' => array('className' => 'Resource', 'dependent' => true));
+
+	public function isAvailable($roomId, $startDatetime, $endDatetime) {
+		$reservation = ClassRegistry::init('Reservation');
+
+		$results = $reservation
+				->find('count',
+						array(
+								'conditions' => array(
+										'Reservation.room_id' => $roomId,
+										'Reservation.end_time >' => $startDatetime,
+										'Reservation.start_time <' => $endDatetime,
+										'Reservation.is_activated' => 1)));
+		if ($results) {
+			return false;
+		}
+		return true;
+	}
 }
