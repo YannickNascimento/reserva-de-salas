f56c140b185342930b144db2ba65dafe317791ee
claudivan.rbr@gmail.com <claudivan.rbr@gmail.com@74c57c3f-0736-1014-c645-9ee079910342>
2012-06-15 17:17:49 +0000

    Formatação

--- reservadesalas/app/Controller/ReservationsController.php
+++ reservadesalas/app/Controller/ReservationsController.php
@@ -15,7 +15,8 @@ class ReservationsController extends AppController {
 		$this->Room = ClassRegistry::init('Room');
 		$this->Resource = ClassRegistry::init('Resource');
 		$this->Reservation = ClassRegistry::init('Reservation');
-		$this->ReservationsResource = ClassRegistry::init('ReservationsResource');
+		$this->ReservationsResource = ClassRegistry::init(
+				'ReservationsResource');
 	}
 
 	public function isAuthorized($user) {
@@ -26,41 +27,59 @@ class ReservationsController extends AppController {
 	}
 
 	public function createReservation($roomId, $date, $startTime, $endTime) {
-		$startDatetime = DateTime::createFromFormat('d-m-Y G-i', $date.' '.$startTime);
-		$displayStart = $startDatetime->format('d/m/Y' .  __(' à\s ') . 'G:i');
+		$startDatetime = DateTime::createFromFormat('d-m-Y G-i',
+				$date . ' ' . $startTime);
+		$displayStart = $startDatetime->format('d/m/Y' . __(' à\s ') . 'G:i');
 		$startDatetime = $startDatetime->format('Y-m-d G:i:s');
-		
-		$endDatetime = DateTime::createFromFormat('d-m-Y G-i', $date.' '.$endTime);
-		$displayEnd = $endDatetime->format('d/m/Y' .  __(' à\s ') . 'G:i');
+
+		$endDatetime = DateTime::createFromFormat('d-m-Y G-i',
+				$date . ' ' . $endTime);
+		$displayEnd = $endDatetime->format('d/m/Y' . __(' à\s ') . 'G:i');
 		$endDatetime = $endDatetime->format('Y-m-d G:i:s');
-		
+
 		if ($this->request->is('post')) {
 			$user = $this->getLoggedUser();
 			$this->request->data['Reservation']['user_id'] = $user['id'];
 			// TODO: Verificar se é usuário comum ou não
 			$this->request->data['Reservation']['is_activated'] = 1;
-			
-			if (! $this->Room->isAvailable($roomId, $startDatetime, $endDatetime) ) {
-				$this->Session->setFlash(__('Sala não disponível. Selecione outra sala.'), 'default', array('class' => 'message errorMessage roundedBorders'));
-				$this->redirect(array('controller' => 'Reservations', 'action' => 'chooseDate'));
+
+			if (!$this->Room
+					->isAvailable($roomId, $startDatetime, $endDatetime)) {
+				$this->Session
+						->setFlash(
+								__('Sala não disponível. Selecione outra sala.'),
+								'default',
+								array(
+										'class' => 'message errorMessage roundedBorders'));
+				$this
+						->redirect(
+								array('controller' => 'Reservations',
+										'action' => 'chooseDate'));
 			}
-			
 			if ($this->Reservation->save($this->request->data)) {
-				$this->Session->setFlash(__('Reserva realizada com sucesso'), 'default', array('class' => 'message success roundedBorders'));
-				$this->redirect(array('controller' => 'Rooms', 'action' => 'viewRoom', $roomId));
+				$this->Session
+						->setFlash(__('Reserva realizada com sucesso'),
+								'default',
+								array(
+										'class' => 'message success roundedBorders'));
+				$this
+						->redirect(
+								array('controller' => 'Rooms',
+										'action' => 'viewRoom', $roomId));
+			} else {
+				$this->Session
+						->setFlash(__('Erro ao reservar sala'), 'default',
+								array(
+										'class' => 'message errorMessage roundedBorders'));
 			}
-			else {
-				$this->Session->setFlash(__('Erro ao reservar sala'), 'default', array('class' => 'message errorMessage roundedBorders'));
-			}
-			
 		}
-		
+
 		$this->set('displayStart', $displayStart);
 		$this->set('displayEnd', $displayEnd);
-		
+
 		$room = $this->Room->findById($roomId);
 		$this->set('room', $room);
-		
+
 		$roomResources = $this->Resource
 				->find('all',
 						array(
@@ -81,24 +100,31 @@ class ReservationsController extends AppController {
 		$date = $param->date;
 		$begin_time = $param->begin_time;
 		$end_time = $param->end_time;
-		
-		$datetime_begin = DateTime::createFromFormat('d/m/Y G:i', $date.' '.$begin_time);
-		$datetime_end = DateTime::createFromFormat('d/m/Y G:i', $date.' '.$end_time);
+
+		$datetime_begin = DateTime::createFromFormat('d/m/Y G:i',
+				$date . ' ' . $begin_time);
+		$datetime_end = DateTime::createFromFormat('d/m/Y G:i',
+				$date . ' ' . $end_time);
 
 		$this->RequestHandler->respondAs('json');
 		$this->autoRender = false;
 
 		$allRooms = $this->Room->find('all');
-		
-		$intersectionTime = array('Reservation.end_time >=' => $datetime_begin->format('Y-m-d G:i:s'),
-								  'Reservation.start_time <=' => $datetime_end->format('Y-m-d G:i:s'),
-								  'Reservation.is_activated' => true);
-		$reservations = $this->Reservation->find('all', array('conditions'=>$intersectionTime));
-		
+
+		$intersectionTime = array(
+				'Reservation.end_time >=' => $datetime_begin
+						->format('Y-m-d G:i:s'),
+				'Reservation.start_time <=' => $datetime_end
+						->format('Y-m-d G:i:s'),
+				'Reservation.is_activated' => true);
+		$reservations = $this->Reservation
+				->find('all', array('conditions' => $intersectionTime));
+
 		/* Filter available Rooms */
-		foreach ($allRooms as $i=>$room) {
-			foreach($reservations as $reservation) {
-				if ($reservation['Reservation']['room_id'] == $room['Room']['id']) {
+		foreach ($allRooms as $i => $room) {
+			foreach ($reservations as $reservation) {
+				if ($reservation['Reservation']['room_id']
+						== $room['Room']['id']) {
 					unset($allRooms[$i]);
 					break;
 				}
